e814f8ea76196557092aa2c444d6b45d0ffcbfb6
Leonardo Cossutta <leonardo.cossutta@gmail.com>
2022-06-17 16:46:25 +0200

    Update requests.js

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,25 +1,64 @@
 import axios from "axios";
 
 class Requests {
-  static BACKEND_URL = "/api/";
-
   static createMenu(onSuccess, onError) {
-    axios
-      .get(`${Requests.BACKEND_URL}menu`)
-      .then((res) => onSuccess(res))
-      .catch((err) => onError(err));
+    Requests.createMenu_vercel(onSuccess, onError);
   }
 
   static addToMenu(id, orders, onSuccess, onError) {
+    Requests.addToMenu_vercel(id, orders, onSuccess, onError);
+  }
+
+  static getMenu(id, onSuccess, onError) {
+    Requests.getMenu_vercel(id, onSuccess, onError);
+  }
+
+  /* VERCEL */
+
+  static BACKEND_URL_1 = "/api/";
+
+  static createMenu_vercel(onSuccess, onError) {
     axios
-      .post(`${Requests.BACKEND_URL}menu/${id}`, { orders })
+      .get(`${Requests.BACKEND_URL_1}menu`)
+      .then((res) => onSuccess(res))
+      .catch(() => Requests.createMenu_netlify(onSuccess, onError));
+  }
+
+  static addToMenu_vercel(id, orders, onSuccess, onError) {
+    axios
+      .post(`${Requests.BACKEND_URL_1}menu/${id}`, { orders })
+      .then((res) => onSuccess(res))
+      .catch(() => Requests.addToMenu_netlify(id, orders, onSuccess, onError));
+  }
+
+  static getMenu_vercel(id, onSuccess, onError) {
+    axios
+      .get(`${Requests.BACKEND_URL_1}menu/${id}`)
+      .then((res) => onSuccess(res))
+      .catch(() => Requests.getMenu_netlify(id, onSuccess, onError));
+  }
+
+  /* NETLIFY */
+
+  static BACKEND_URL_2 = "/.netlify/functions/server/";
+
+  static createMenu_netlify(onSuccess, onError) {
+    axios
+      .get(`${Requests.BACKEND_URL_2}menu`)
       .then((res) => onSuccess(res))
       .catch((err) => onError(err));
   }
 
-  static getMenu(id, onSuccess, onError) {
+  static addToMenu_netlify(id, orders, onSuccess, onError) {
     axios
-      .get(`${Requests.BACKEND_URL}menu/${id}`)
+      .post(`${Requests.BACKEND_URL_2}menu/${id}`, { orders })
+      .then((res) => onSuccess(res))
+      .catch((err) => onError(err));
+  }
+
+  static getMenu_netlify(id, onSuccess, onError) {
+    axios
+      .get(`${Requests.BACKEND_URL_2}menu/${id}`)
       .then((res) => onSuccess(res))
       .catch((err) => onError(err));
   }
